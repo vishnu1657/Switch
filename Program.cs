5e2303c7a1ae1c0245055f6cacb2aaf06214e1b6
HP <HP@DESKTOP-4Q8V1JF>
2023-03-17 00:52:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+            switch(age)
+            {
+                case 18:
+                    Console.WriteLine("please wait an year");
+                    break;
+
+                    case 19:
+                    Console.WriteLine("you are 19");
+                    break;
+
+                    case 20:
+                    Console.WriteLine("you are 20");
+                    break;
+
+                    Default:
+                    Console.WriteLine("Enjoy!");
+                    break;
+
+                    Console.ReadLine();
+
+            } 
+
+        }
+    }
+}
